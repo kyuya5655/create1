df1242340337cd629eab1d53ebbad0fa457f257b
YuyaKouguchi <134293910+kyuya5655@users.noreply.github.com>
2023-07-08 09:24:57 +0000

    20230708_1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,16 +5,21 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    if @list.save
+    # １. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    if list.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿が成功しました"
       redirect_to list_path(list.id)
     else
-      render :index
+      render :new
     end
   end
 
   def index
     @lists = List.all
+    byebug
   end
 
   def show
